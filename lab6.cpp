9bb7946650b910d6695706c35cc817eb70619c9c
Dan <daniyar.burakaev@mail.ru>
2019-11-11 05:20:07 +0300

    Even better...

--- lab6.cpp
+++ lab6.cpp
@@ -1,20 +1,25 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <signal.h>
+#include <wait.h>
 #include <iostream>
 
 using namespace std;
 
+int LAST_PID;
+
 void subrun(int i) {
   if(fork() == 0) {
+    LAST_PID = getpid();
     timeval curtime;
 
     gettimeofday(&curtime, NULL);
-    cout << ctime(&curtime.tv_sec) << "Subprocess PID: " << getpid() << endl << endl;
+    cout << "Subprocess PID: " << getpid() << endl << ctime(&curtime.tv_sec) << endl;
 
+    exit(EXIT_SUCCESS);
   }
 
-    // execl("./sub_prog", argv[0], (char*) 0);
+  // execl("./sub_prog", argv[0], (char*) 0);
 }
 
 int main(int argc, char const *argv[]) {
@@ -25,7 +30,7 @@ int main(int argc, char const *argv[]) {
   int n_of_runs;
   timeval per;
   itimerval timer, timer_c;
-  time_t s, e;
+  time_t pr_s = time(NULL), sub_s, rt_s;
 
   try {
     n_of_runs = stoi(argv[1]);
@@ -55,16 +60,18 @@ int main(int argc, char const *argv[]) {
   setitimer(ITIMER_REAL, &timer, NULL);
   for(int i = 0; i < n_of_runs; i++) {
     // cout << "\n[RUN]\n";
-    s = time(NULL);
+    rt_s = time(NULL);
     pause();
 
     // sigset_t t; int p;
     // sigaddset(&t, SIGALRM);
     // sigwait(&t, &p);
 
-    e = time(NULL);
-    cout << "Real time period: " << difftime(e, s) << endl;
+    cout << "Real time period: " << difftime(time(NULL), rt_s) << endl;
   }
+
+  usleep(1000);
+  cout << "\n- - -\nProcess working time: " << difftime(time(NULL), pr_s) << endl;
 
   return 0;
 }
